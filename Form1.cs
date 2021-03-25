6f3a8921f8bb92b0df2b551255b2184cafd98c2c
durga750 <78526756+durga750@users.noreply.github.com>
2021-03-25 10:52:22 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            label3.Text = "Sum is" + new ServiceReference1.WebService1SoapClient().Sum(a, b).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            label3.Text = "Sub is" + new ServiceReference1.WebService1SoapClient().Sub(a, b).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            label3.Text = "Mul is" + new ServiceReference1.WebService1SoapClient().Mul(a, b).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            label3.Text = "Div is" + new ServiceReference1.WebService1SoapClient().Div(a, b).ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            label3.Text = "Modl is" + new ServiceReference1.WebService1SoapClient().Modl(a, b).ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label3.Text = new ServiceReference1.WebService1SoapClient().HelloWorld().ToString();
+        }
+    }
+}
---
+++ MyService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfDemoService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MyService" in both code and config file together.
+    public class MyService : IMyService
+    {
+        public string GetMessage(string Name)
+        {
+            return $"Hi {Name}";
+        }
+    }
+}
